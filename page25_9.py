4c114d76826c7067a5d5f5cb06ca5a70da87b7b8
python290821 <90146835+python290821@users.noreply.github.com>
2021-10-13 18:11:31 +0300

    Create page25_9.py

---
+++ page25_9.py
@@ -0,0 +1,17 @@
+
+x = int(input('please enter a number'))
+biggest = x
+index_biggest = 1
+index = 1
+
+while index < 3:
+    y[index]
+    index += 1
+    x = int(input('please enter a number'))
+    if x > biggest:
+        biggest = x
+        index_biggest = index
+
+#print('the biggest number #', index_biggest,': value-', biggest)
+print(f'the biggest number #{index_biggest}: value-{biggest} {y}')
+# the biggest number #12: value-800
