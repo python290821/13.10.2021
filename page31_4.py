c8cda3c48e233197592e559480984883109194ea
python290821 <90146835+python290821@users.noreply.github.com>
2021-10-13 20:43:32 +0300

    Create page31_4.py

---
+++ page31_4.py
@@ -0,0 +1,12 @@
+
+n = int(input('input number'))
+_sum = 0
+for i in range(3,            n + 1 ,           3):
+    #          i = 3    while i < n + 1     i += 3
+    _sum += i
+print(_sum)
+
+# i = 3
+# while i < n + 1
+#   sum += i
+#   i += 3
